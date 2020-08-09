c265f3d4a7e3a8e737d7853c8160bc901c52c839
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-08-09 12:02:53 -0600

    MainActivity se agrego funcionalidad al datePicker

--- app/src/main/java/com/waltergaldamezweb/formulario/MainActivity.java
+++ app/src/main/java/com/waltergaldamezweb/formulario/MainActivity.java
@@ -1,14 +1,54 @@
 package com.waltergaldamezweb.formulario;
+import com.waltergaldamezweb.formulario.DatePickerFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText edtFechaN;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtFechaN = (TextInputEditText) findViewById(R.id.edtFechaN);
+        edtFechaN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.edtFechaN:
+                        showDatePickerDialog();
+                        break;
+                }
+            }
+        });
     }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = day + " / " + (month+1) + " / " + year;
+                edtFechaN.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
+
 }
